155629f6153df4a87d051877c63b8166da306dac
Tobias Hildebrandt <hildebrandt.tobias@gmail.com>
2022-11-05 14:42:32 +0100

    Socket - trying bug fix again

--- Niblette.py
+++ Niblette.py
@@ -159,6 +159,7 @@ class Niblette(irc.bot.SingleServerIRCBot):
         peer_port = int(peer_port)
         self.downloader = self.dcc("raw")
         self.downloader = self.downloader.connect(peer_address, peer_port)
+        set_keepalive_linux(self.downloader.socket)
 
     def send_bytes(self, bytes):
         try:
@@ -249,6 +250,19 @@ class Niblette(irc.bot.SingleServerIRCBot):
     #     :param pubmsg: The whole public message text that announced a new upload
     #     :return: The name of the show that was being uploaded.
     #     """
+
+
+def set_keepalive_linux(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
+    """Set TCP keepalive on an open socket.
+
+    It activates after 1 second (after_idle_sec) of idleness,
+    then sends a keepalive ping once every 3 seconds (interval_sec),
+    and closes the connection after 5 failed ping (max_fails), or 15 seconds
+    """
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
 
 def determineStructure(filename):
     nameMatch = Niblette.showPattern.findall(filename)
