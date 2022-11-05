3be18a12d5c04566f3fe0dc05195f8b2fb319125
Tobias Hildebrandt <hildebrandt.tobias@gmail.com>
2022-11-05 15:57:12 +0100

    Socket - trying bug fix again

--- Niblette.py
+++ Niblette.py
@@ -201,7 +201,7 @@ class Niblette(irc.bot.SingleServerIRCBot):
 
             # connection.send_bytes(struct.pack("!I", self.received_bytes))
             self.send_bytes(struct.pack("!I", self.received_bytes))
-            time.sleep(1 / 250.0)
+            time.sleep(1 / 200.0)
 
 
             if (self.received_bytes == self.total_bytes):
