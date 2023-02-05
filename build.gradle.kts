34eb8afcd65ac70ead6348de7ac6aa9d7ac210f8
Tobias Hildebrandt <hildebrandt.tobias@gmail.com>
2023-02-05 02:24:33 +0100

    Changed the way that pircbotx dependency is loaded to make it shadowJar compatible.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ group = "me.hilde"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    flatDir {
+        dirs("/lib")
+    }
     mavenCentral()
 }
 
@@ -28,10 +31,11 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-    implementation(files("/lib/pircbotx/pircbotx.jar"))
     implementation("org.slf4j:slf4j-api:2.0.6")
     implementation("org.slf4j:slf4j-simple:2.0.6")
     implementation("ch.qos.logback:logback-core:1.4.5")
+    implementation("org.pircbotx:pircbotx:2.3")
+//    implementation(files("/lib/pircbotx/pircbotx.jar"))
 }
 
 tasks.test {
