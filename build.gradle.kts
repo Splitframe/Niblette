e7da56c7c4762fdf1bc6479f025126181aca7d69
Tobias Hildebrandt <hildebrandt.tobias@gmail.com>
2023-02-05 01:46:32 +0100

    Set Kotlin JVM to 1.8.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    kotlin("jvm") version "19"
+    kotlin("jvm") version "1.8.10"
     application
 }
 
